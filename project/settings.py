5787c9f04c1c69adcc553e9be1b92c7d83ae6f7a
Jos Bogan <Josbogan@gmail.com>
2020-11-02 15:52:19 +0000

    help

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-from secrets import SECRET_KEY
+from .secret_settings import *
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
